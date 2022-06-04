2a73def9fb9aecc8317bba3556444eb238f0b3c2
UnaryPlus <ombspring@gmail.com>
2022-06-03 22:25:55 -0400

    add skip button

--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,12 @@ function sketch(instance:p5) {
 
   let creatures:Creature[]
   let deleted:boolean[]
+
   let mainButton:p5.Element
+  let skipButton:p5.Element
   let backButton:p5.Element
   let resetButton:p5.Element
+
   let action:Action
   let zoomed:Creature|null = null
   let generation:number = 0
@@ -36,6 +39,11 @@ function sketch(instance:p5) {
     mainButton.mouseClicked(mainClicked)
     mainButton.elt.onselectstart = () => false
     action = 'sort'
+
+    skipButton = p.createButton("Skip 10 generations")
+    skipButton.position(620, 350)
+    skipButton.mouseClicked(skipClicked)
+    skipButton.elt.onselectstart = () => false
 
     backButton = p.createButton("Back")
     backButton.position(20, 20)
@@ -68,6 +76,7 @@ function sketch(instance:p5) {
       zoomed = getCreature(creatures)
       if(zoomed !== null) {
         mainButton.hide()
+        skipButton.hide()
         backButton.show()
         resetButton.show()
       }
@@ -77,6 +86,7 @@ function sketch(instance:p5) {
   function mainClicked() : void {
     if(action === 'sort') {
       sortByFitness(creatures)
+      skipButton.hide()
       mainButton.html('Filter population')
       action = 'filter'
     }
@@ -89,6 +99,7 @@ function sketch(instance:p5) {
       creatures = createOffspring(creatures, deleted)
       deleted = Array.from({ length:100 }, () => false)
       generation += 1
+      skipButton.show()
       mainButton.html('Sort by fitness')
       action = 'sort'
       drawStatistics(creatures)
@@ -97,12 +108,26 @@ function sketch(instance:p5) {
     drawHeading(generation, action)
   }
 
+  function skipClicked() : void {
+    for(let i = 0; i < 10; i++) {
+      sortByFitness(creatures)
+      deleted = filterGradient(creatures)
+      creatures = createOffspring(creatures, deleted)
+      deleted = Array.from({ length:100 }, () => false)
+    }
+    generation += 10
+    drawGrid(creatures, deleted)
+    drawHeading(generation, action)
+    drawStatistics(creatures)
+  }
+
   function backClicked() : void {
     if(zoomed !== null) zoomed.reset()
     zoomed = null
     backButton.hide()
     resetButton.hide()
     mainButton.show()
+    if(action === 'sort') skipButton.show()
     p.background(255)
     drawGrid(creatures, deleted)
     drawHeading(generation, action)
